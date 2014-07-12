9de857c981e0bfc3b2872140ba396ec0b747e0b1
Parker Smith <parker@parkersmith.us>
2014-07-12 15:26:05 -0600

    Add laravel file decouplr

---
+++ src/File/LaravelFileDecouplr.php
@@ -0,0 +1,137 @@
+<?php namespace LaravelDecouplr\File; 
+
+use Decouplr\Decouplr;
+use Illuminate\Filesystem\Filesystem;
+
+class LaravelFileDecouplr extends Decouplr {
+
+    public function __construct(Filesystem $files)
+    {
+        $this->decoupled = $files;
+    }
+
+    public function exists()
+    {
+        return $this->delegate(__FUNCTION__, func_get_args());
+    }
+
+    public function get()
+    {
+        return $this->delegate(__FUNCTION__, func_get_args());
+    }
+
+    public function getRequire()
+    {
+        return $this->delegate(__FUNCTION__, func_get_args());
+    }
+
+    public function requireOnce()
+    {
+        return $this->delegate(__FUNCTION__, func_get_args());
+    }
+
+    public function put()
+    {
+        return $this->delegate(__FUNCTION__, func_get_args());
+    }
+
+    public function prepend()
+    {
+        return $this->delegate(__FUNCTION__, func_get_args());
+    }
+
+    public function append()
+    {
+        return $this->delegate(__FUNCTION__, func_get_args());
+    }
+
+    public function delete()
+    {
+        return $this->delegate(__FUNCTION__, func_get_args());
+    }
+
+    public function move()
+    {
+        return $this->delegate(__FUNCTION__, func_get_args());
+    }
+
+    public function copy()
+    {
+        return $this->delegate(__FUNCTION__, func_get_args());
+    }
+
+    public function extension()
+    {
+        return $this->delegate(__FUNCTION__, func_get_args());
+    }
+
+    public function type()
+    {
+        return $this->delegate(__FUNCTION__, func_get_args());
+    }
+
+    public function size()
+    {
+        return $this->delegate(__FUNCTION__, func_get_args());
+    }
+
+    public function lastModified()
+    {
+        return $this->delegate(__FUNCTION__, func_get_args());
+    }
+
+    public function isDirectory()
+    {
+        return $this->delegate(__FUNCTION__, func_get_args());
+    }
+
+    public function isWritable()
+    {
+        return $this->delegate(__FUNCTION__, func_get_args());
+    }
+
+    public function isFile()
+    {
+        return $this->delegate(__FUNCTION__, func_get_args());
+    }
+
+    public function glob()
+    {
+        return $this->delegate(__FUNCTION__, func_get_args());
+    }
+
+    public function files()
+    {
+        return $this->delegate(__FUNCTION__, func_get_args());
+    }
+
+    public function allFiles()
+    {
+        return $this->delegate(__FUNCTION__, func_get_args());
+    }
+
+    public function directories()
+    {
+        return $this->delegate(__FUNCTION__, func_get_args());
+    }
+
+    public function makeDirectory()
+    {
+        return $this->delegate(__FUNCTION__, func_get_args());
+    }
+
+    public function copyDirectory()
+    {
+        return $this->delegate(__FUNCTION__, func_get_args());
+    }
+
+    public function deleteDirectory()
+    {
+        return $this->delegate(__FUNCTION__, func_get_args());
+    }
+
+    public function cleanDirectory()
+    {
+        return $this->delegate(__FUNCTION__, func_get_args());
+    }
+} 
