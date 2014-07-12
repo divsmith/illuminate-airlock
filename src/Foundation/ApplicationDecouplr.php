24b7dca563894e9db8c7842ae6d9fd0eab3ab9d6
Parker Smith <parker@parkersmith.us>
2014-07-12 15:42:14 -0600

    Make class abstract

--- src/Foundation/ApplicationDecouplr.php
+++ src/Foundation/ApplicationDecouplr.php
@@ -3,7 +3,7 @@
 use Decouplr\Decouplr;
 use Illuminate\Foundation\Application;
 
-class ApplicationDecouplr {
+abstract class ApplicationDecouplr {
 
     public function __construct(Application $app)
     {
